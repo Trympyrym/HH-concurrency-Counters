236a7ce3b7da8da0338e80722637fc7ad5686296
Trympyrym <rio09876@gmail.com>
2017-01-18 17:24:22 +0200

    reworked from LinkedQueue to HashMap

--- src/CountersImpl.java
+++ src/CountersImpl.java
@@ -1,53 +1,32 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by trympyrym on 13.01.17.
  *
- *  ConcurrentLinkedQueue предоствляет параллельные потокобезопасные операции
- *  добавления элемента в конец очереди и удаления его из начала.
- *  Таким образом, множество потоков, выполняющих increment не будут мешать
- *  друг другу и poll-у из метода getCountersAndClear.
- *  Метод getCountersAndClear берет по одному элементу с начала очереди и
- *  формирует результат. Подсчет производится именно тут, чтобы разгрузить
- *  часто используемый метод increment. Параллельная работа двух getCountersAndClear
- *  сделает очень плохо, поэтому поставлен дополнительный synchronized блок.
  */
 public class CountersImpl implements Counters {
-    private ConcurrentLinkedQueue<String> tags = new ConcurrentLinkedQueue<>();
-    private Object Lock = new Object();
+    private ConcurrentMap<String, AtomicInteger> statistics = new ConcurrentHashMap<>();
 
     @Override
-    public void increment(String tag) {
-        tags.add(tag);
+    public void increment(String tag)
+    {
+        statistics.putIfAbsent(tag, new AtomicInteger(0));
+        statistics.get(tag).incrementAndGet();
     }
-
-//    @Override
-//    public Map<String, Long> getCountersAndClear() {
-//        Map<String, Long> result = new HashMap<>();
-//        for (String tag: tags) {
-//            Long prevValue = (result.containsKey(tag)) ? result.get(tag) : new Long(0);
-//            Long newValue = prevValue + 1;
-//            result.put(tag, newValue);
-//            tags.poll();
-//        }
-//        return result;
-//    }
 
     @Override
     public Map<String, Long> getCountersAndClear() {
-        synchronized (Lock)
+        Map<String, Long> result = new HashMap<>();
+        for (ConcurrentMap.Entry<String, AtomicInteger> entry : statistics.entrySet())
         {
-            Long size = (long)tags.size();
-            Map<String, Long> result = new HashMap<>();
-            for (long index = 0; index < size; index++) {
-                String tag = tags.poll();
-                Long prevValue = (result.containsKey(tag)) ? result.get(tag) : new Long(0);
-                Long newValue = prevValue + 1;
-                result.put(tag, newValue);
-            }
-            return result;
+            result.put(entry.getKey(), entry.getValue().longValue());
+            statistics.get(entry.getKey()).addAndGet((int)-entry.getValue().longValue());
+            statistics.remove(entry.getKey(), 0);
         }
+        return result;
     }
 }
